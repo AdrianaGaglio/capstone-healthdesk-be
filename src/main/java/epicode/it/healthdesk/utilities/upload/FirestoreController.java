ba8f8729349a86c2594bfded91f8a93ad8e9770d
Adriana Gaglio <adriana.gaglio@gmail.com>
2025-02-04 18:33:25 +0100

    wip upload

--- src/main/java/epicode/it/healthdesk/utilities/upload/FirestoreController.java
+++ src/main/java/epicode/it/healthdesk/utilities/upload/FirestoreController.java
@@ -3,6 +3,7 @@ package epicode.it.healthdesk.utilities.upload;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,10 +13,12 @@ import org.springframework.web.multipart.MultipartFile;
 @RestController
 @RequestMapping("/api/files")
 @RequiredArgsConstructor
+@PreAuthorize("isAuthenticated()")
 public class FirestoreController {
     private final FirestoreSvc firestoreSvc;
 
-    @PostMapping(path="/upload-prescription", consumes = "multipart/form-data")
+    @PostMapping(path = "/upload-prescription", consumes = "multipart/form-data")
+    @PreAuthorize("hasRole('ADMIN', 'DOCTOR')")
     public ResponseEntity<String> uploadPrescription(@RequestParam("file") MultipartFile file) {
         try {
             String fileUrl = firestoreSvc.uploadPrescription(file);
@@ -25,7 +28,7 @@ public class FirestoreController {
         }
     }
 
-    @PostMapping(path="/upload-image", consumes = "multipart/form-data")
+    @PostMapping(path = "/upload-image", consumes = "multipart/form-data")
     public ResponseEntity<String> uploadImage(@RequestParam("file") MultipartFile file) {
         try {
             String fileUrl = firestoreSvc.uploadPrescription(file);
